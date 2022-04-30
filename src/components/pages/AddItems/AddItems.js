04351af4a5eee42979df37636fa0f834f2654551
Md Mazharul islam <miforbd@gmail.com>
2022-04-30 23:44:54 +0600

    add item sent data database added

--- src/components/pages/AddItems/AddItems.js
+++ src/components/pages/AddItems/AddItems.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import Header from '../../Home/Header/Header';
 
 const AddItems = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
+        const url = `https://vehicle-storehouse.herokuapp.com/item`
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "content-type": 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+            })
+    }
     return (
         <div>
-            <Header/>
-            <h2>Add items page</h2>
+            <Header />
+            <h2 className='text-center my-5 text-4xl'>Add item</h2>
+            <form className='w-3/5 mx-auto flex flex-col gap-2' onSubmit={handleSubmit(onSubmit)}>
+                <input className='border px-3 py-1 mb-3 w-full bg-gray-50' {...register("name", { required: true})} placeholder="Enter Car Name"/>
+                <input className='border px-3 py-1 mb-3 w-full bg-gray-50' {...register("img", { required: true})} placeholder="Enter Image link"/>
+                <input className='border px-3 py-1 mb-3 w-full bg-gray-50' type="number" {...register("price",)} placeholder="Enter Car price"/>
+                <textarea className='border px-3 py-1 mb-3 w-full bg-gray-50' {...register("des", { required: true})} placeholder="Enter Car description"/>
+                <input className='border px-3 py-1 mb-3 w-full bg-gray-50' type="number" {...register("quantity",)} placeholder="Enter Car quantity"/>
+                <input className='border px-3 py-1 mb-3 w-full bg-gray-50' {...register("subName", { required: true})} placeholder="Enter Car Supplier Name"/>
+                <input className='border px-3 py-1 mb-3 w-full bg-gray-50' type="email" {...register("email", { required: true})} placeholder="Enter your Email"/>
+                <input className='px-3 py-1 bg-gray-300 hover:bg-gray-400 cursor-pointer rounded-md w-full' type="submit" />
+            </form>
         </div>
     );
 };
