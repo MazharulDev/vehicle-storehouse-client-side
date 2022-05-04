76c56af27d8dd149fa18f51fe818ff443459f6b8
Md Mazharul islam <miforbd@gmail.com>
2022-05-04 11:04:11 +0600

    countup section added

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NewsLetter from '../../pages/NewsLetter/NewsLetter';
 import Banner from '../Banner/Banner';
+import CountSection from '../CountSection/CountSection';
 import Footer from '../Header/Footer/Footer';
 import Header from '../Header/Header';
 import Items from '../Items/Items';
@@ -11,6 +12,7 @@ const Home = () => {
             <Header/>
             <Banner/>
             <Items/>
+            <CountSection/>
             <NewsLetter/>
             <Footer/>
         </div>
--- src/components/pages/NewsLetter/NewsLetter.js
+++ src/components/pages/NewsLetter/NewsLetter.js
@@ -12,13 +12,15 @@ const NewsLetter = () => {
         
     }
     return (
-        <div className='h-64 flex items-center justify-center bg-img-news'>
+        <div className='my-5'>
+            <div className='h-64 flex items-center justify-center bg-img-news'>
             <div className='md:flex items-center justify-center gap-3 bg-text'>
                 <h2 className='text-5xl text-center text-white mb-6 md:mb-0'>Newsletter</h2>
                 <div>
                     <input onChange={handleEmailChange} className='px-5 py-3 border rounded-lg mb-4 md:mb-0' type="email" name="email" placeholder='Your Email Address'/>
                     <button onClick={handleSignUpBtn} className='px-5 py-3 rounded-lg bg-slate-300 text-black ml-3 hover:bg-gray-500 hover:text-white duration-200'>Sign up</button>
                 </div>
+            </div>
             </div>
         </div>
     );
